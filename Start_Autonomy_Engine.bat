289b9af24c59076b7d1b6d7830b44ce6383aa978
Ericdolsen <eric.olsen3@gmail.com>
2026-04-18 15:04:19 -0500

    Auto-commit latest scripts and .bat

---
+++ Start_Autonomy_Engine.bat
@@ -0,0 +1,11 @@
+﻿@echo off
+title Gateway Park Autonomy Engine
+color 0B
+echo ========================================================
+echo           GATEWAY PARK AUTOMATION SERVER
+echo ========================================================
+echo.
+echo Initializing Server...
+cd "C:\Users\erico\Desktop\Cloudbeds-Autonomy-Engine"
+node server.js
+pause
